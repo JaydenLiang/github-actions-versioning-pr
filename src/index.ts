c559a43be78e8dde44e99554ebca839d7369d4d7
Jayden Liang <jaydenliang81@gmail.com>
2021-03-23 10:13:54 -0700

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 import axios, { AxiosRequestConfig } from 'axios';
+import yaml from 'yaml';
 
 
-// interface PrTemplate {
-//     title: string;
-//     description?: string;
-//     preset?: {
-//         reviewers?: string[];
-//         'team-reviewers'?: string[];
-//         labels?: string[];
-//     }
-// }
+interface PrTemplate {
+    title: string;
+    description?: string;
+    preset?: {
+        reviewers?: string[];
+        'team-reviewers'?: string[];
+        labels?: string[];
+    }
+}
 
 async function fetchPackageJson(owner: string, repo: string, branch: string): Promise<{ [key: string]: unknown }> {
     const basePackageJsonUrl = `https://raw.githubusercontent.com/` +
@@ -29,18 +30,21 @@ async function fetchPackageJson(owner: string, repo: string, branch: string): Pr
     return response.data;
 }
 
-// async function fetchPrTemplate(uri: string): Promise<PrTemplate> {
-//     const options: AxiosRequestConfig = {
-//         method: 'GET',
-//         headers: {
-//             Accept: 'text/plain'
-//         },
-//         url: uri,
-//         timeout: 30000
-//     };
-//     const response = await axios(options);
-//     return yamlParse(response.data) as PrTemplate;
-// }
+async function loadPrTemplate(owner: string, repo: string, branch: string, filePath: string): Promise<PrTemplate> {
+    const url = `https://raw.githubusercontent.com/` +
+        `${owner}/${repo}/${branch}/${filePath}`;
+
+    const options: AxiosRequestConfig = {
+        method: 'GET',
+        headers: {
+            Accept: 'text/plain'
+        },
+        url: url,
+        timeout: 30000
+    };
+    const response = await axios(options);
+    return yaml.parse(response.data) as PrTemplate;
+}
 
 async function main(): Promise<void> {
     try {
@@ -48,6 +52,7 @@ async function main(): Promise<void> {
         const [, refType, refName] = /(?<=refs\/)([^\/]*)\/(\S*)/gm.exec(String(github.context.payload.ref)) || [];
         const isBranch = refType === 'heads';
         // const isTag = refType === 'tags';
+        const mainBranch = github.context.payload.repository.default_branch || 'main';
         const baseBranch = core.getInput('base-branch') || '';
         const headBranch = isBranch && refName;
         const isPrerelease = core.getInput('is-prerelease') || '';
@@ -56,12 +61,12 @@ async function main(): Promise<void> {
         const prTemplateUri = core.getInput('pr-template-uri') || '';
         let prTitle: string = core.getInput('pr-title') || '';
         let prDescription: string = core.getInput('pr-description') || '';
-        // const inputPrReviewers = core.getInput('pr-reviewers') || '';
-        // const inputPrTeamReviewers = core.getInput('pr-team-reviewers') || '';
-        // const inputPrLabels = core.getInput('pr-labels') || '';
-        // let prReviewers = inputPrReviewers.split(',') || [];
-        // let prTeamReviewers = inputPrTeamReviewers.split(',') || [];
-        // let prLabels = inputPrLabels.split(',') || [];
+        const inputPrReviewers = core.getInput('pr-reviewers') || '';
+        const inputPrTeamReviewers = core.getInput('pr-team-reviewers') || '';
+        const inputPrLabels = core.getInput('pr-labels') || '';
+        let prReviewers = inputPrReviewers.split(',') || [];
+        let prTeamReviewers = inputPrTeamReviewers.split(',') || [];
+        let prLabels = inputPrLabels.split(',') || [];
         // fetch the old version
         console.log(`Fetching package.json from: ${owner}/${repo}/${baseBranch}`);
         const basePackageJson: { [key: string]: unknown } = await fetchPackageJson(owner, repo, baseBranch);
@@ -74,20 +79,25 @@ async function main(): Promise<void> {
         if (prTemplateUri) {
             console.log('prTemplateUri:', prTemplateUri);
             console.log('cwd', process.cwd());
-            // const templateYaml = await fetchPrTemplate(prTemplateUri);
-            // prTitle = prTitle || (templateYaml.title);
-            // prDescription = prDescription || templateYaml.description;
-            // // NOTE: optional chaining works in NodeJS 12 and higher version only
-            // if (prReviewers.length === 0 && templateYaml.preset?.reviewers) {
-            //     prReviewers = templateYaml.preset.reviewers;
-            // }
-            // if (prTeamReviewers.length === 0 && templateYaml.preset?.['team-reviewers']) {
-            //     prTeamReviewers = templateYaml.preset['team-reviewers'];
-            // }
-            // if (prLabels.length === 0 && templateYaml.preset?.labels) {
-            //     prLabels = templateYaml.preset.labels;
-            // }
+            // NOTE: the template must reside in your GitHub repository, either in
+            // the default branch or the head branch
+            const templateYaml = await loadPrTemplate(owner, repo, mainBranch, prTemplateUri);
+            prTitle = prTitle || (templateYaml.title);
+            prDescription = prDescription || templateYaml.description;
+            // NOTE: optional chaining works in NodeJS 12 and higher version only
+            if (prReviewers.length === 0 && templateYaml.preset?.reviewers) {
+                prReviewers = templateYaml.preset.reviewers;
+            }
+            if (prTeamReviewers.length === 0 && templateYaml.preset?.['team-reviewers']) {
+                prTeamReviewers = templateYaml.preset['team-reviewers'];
+            }
+            if (prLabels.length === 0 && templateYaml.preset?.labels) {
+                prLabels = templateYaml.preset.labels;
+            }
         }
+        console.log('prReviewers:', prReviewers);
+        console.log('prTeamReviewers:', prTeamReviewers);
+        console.log('prLabels:', prLabels);
         // replace place holders in template
         const replace = (str: string): string => {
             return str
